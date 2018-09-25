0d5929ba165f28ac71eaf10de5a5d307c0a1fdab
jacky <lqdcims@sina.cn>
2018-09-25 13:55:23 +0800

    wallet
    waller for exchange digital asset

---
+++ src/libs/server.js
@@ -0,0 +1,22 @@
+import insight from 'libs/nodes/insight'
+import qtumInfo from 'libs/nodes/qtumInfo'
+
+let nodeConfigs = {
+  insight,
+  qtumInfo
+}
+
+const defaultNodeId = 'qtumInfo'
+let currentNodeId = defaultNodeId
+
+export default {
+  currentNode() {
+    return nodeConfigs[currentNodeId]
+  },
+
+  setNodeId(nodeId) {
+    if (nodeConfigs[nodeId]) {
+      currentNodeId = nodeId
+    }
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import VueClipboard from 'vue-clipboard2'
+import Vuetify from 'vuetify'
+import App from 'App'
+
+import 'vuetify/dist/vuetify.min.css'
+import 'assets/less/qtum-icons.less'
+import 'assets/css/material-icons.css'
+import 'assets/css/main.css'
+
+Vue.use(Vuetify)
+Vue.use(VueClipboard)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue(App).$mount('#app')
